946aaec1d8516f230ceb92bccfaebdc3ea2f0c1b
Hippolyte EZIAN <polarusdev@gmail.com>
2023-03-06 21:10:37 +0000

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Personne;
+use Faker\Provider\sr_Cyrl_RS\Person;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function displayHome()
+    {
+        $p = new Personne();
+        $p->name = "Yassine";
+        return view("home", ['person' => $p]);
+    }
+}
