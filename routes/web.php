306a94509f6a79ae594ca62c01e4aa989794bac5
Hippolyte EZIAN <polarusdev@gmail.com>
2023-03-11 10:17:56 +0000

    Reset project

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get("/accueil", [HomeController::class, "displayHome"]);
-Route::get("/addPost", [PostController::class, 'create']);
-Route::post("/savePost", [PostController::class, 'store']);
-Route::put("/updatePost/{id}", [PostController::class, 'update']);
-Route::get("/listPost", [PostController::class, 'index']);
-Route::get("/editPost/{id}", [PostController::class, 'edit']);
-Route::delete("/deletePost/{id}", [PostController::class, 'destroy']);
